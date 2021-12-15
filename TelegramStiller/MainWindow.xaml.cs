5d1f261cf110ab2f8e0459715dedd8e6d609d061
Abdulaziz <abdu3332099@gmail.com>
2021-12-15 12:10:55 +0500

    ignore

---
+++ TelegramStiller/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TelegramStiller
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //SetAttributes("api", FileAttributes.Hidden, FileAttributes.System, FileAttributes.ReadOnly);
+            Process[] Telegram = Process.GetProcesses();
+            
+
+            MessageBox.Show(Telegram.Length.ToString());
+        }
+
+        public void SetAttributes(string path, params FileAttributes[] attributes)
+        {
+            foreach (var attribute in attributes)
+            {
+                File.SetAttributes(path, attribute);
+            }
+        }
+    }
+}
